c2299ac7fe3b53ddfe9439c4936f0587408c0a2c
navsingh3211 <navsingh3211@gmail.com>
2024-03-01 18:41:21 +0530

    404 page

--- first-app/src/pages/404.js
+++ first-app/src/pages/404.js
@@ -1,26 +1,36 @@
-// import React from "react";
-// import error from "@/styles/Error.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const ErrorPage = () => {
+  const router = useRouter();
+  const onClickHandler = ()=>{
+    router.push("/");
+  }
   return (
     <>
       <section className="page_404">
         <div className="container">
           <div className="row">
             <div className="col-sm-12 ">
-              <div className="col-sm-10 col-sm-offset-1  text-center" style={{ textAlign:"center"}}> 
+              <div
+                className="col-sm-10 col-sm-offset-1  text-center"
+                style={{ textAlign: "center" }}
+              >
                 <div className="four_zero_four_bg">
-                  <h1 >404</h1>
+                  <h1>404</h1>
                 </div>
 
                 <div className="contant_box_404">
                   <h3 className="h2">Look like you're lost</h3>
 
                   <p>the page you are looking for not avaible!</p>
-
-                  <a href="/" className="link_404">
+                  {/* <Link href="/" className="link_404">
                     Go to Home
-                  </a>
+                  </Link> */}
+
+                   {/* <a onClick={()=>router.push("/")}  className="link_404">Go to Home</a> */}
+
+                   <a onClick={onClickHandler}  className="link_404">Go to Home</a>
                 </div>
               </div>
             </div>
